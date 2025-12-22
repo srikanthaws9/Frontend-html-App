fd698d693b290c46f58a1f85b55251b96a3004f1
reddyramay <101283610+reddyramay@users.noreply.github.com>
2025-12-22 09:00:47 -0800

    committed

---
+++ Python/multiline.py
@@ -0,0 +1,20 @@
+Subjects="""Telugu
+Hindi   
+English
+Maths
+Science
+Social
+Computer
+Physical Education
+Art
+Music
+Dance
+Moral Science
+Health Education
+Environmental Studies
+Civics
+Geography"""
+#print(Subjects)
+print("Total Subjects:", len(Subjects.splitlines()))    
+print("First Subject:", Subjects.splitlines()[0])
+print("Last Subject:", Subjects.splitlines()[1])   
